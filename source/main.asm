bd7d8f82c2c96634d5eec8dab4767cc4f873d188
Mathis-tempo <mathistempo78@gmail.com>
2024-09-26 23:44:53 +0200

    Main function, print function, can now support arguments

--- source/main.asm
+++ source/main.asm
@@ -1,65 +1,83 @@
 section .data
-	filename db "/bin/sh", 0
 	buf times 4 db 0            ; Buffer for the 4 first bytes
 	ELF_numbers db 0x7F, 0x45, 0x4C , 0x46  
 	is_ELF_msg db "The file is an ELF file.", 0xA, 0   ;0xA is for line break
-	not_ELF_msg db "The file is not an ELF file.", 0xA, 0
-	is_Directory_msg db "The file is a Directory", 0xA, 0   ;0xA is for line break
-	not_Directory_msg db "The file is not a Directory", 0xA, 0
+	not_ELF_msg db "error, the file printed is not an ELF file.", 0xA, 0
 	error_msg db "The programm returned an error", 0xA, 0
+	error_msg_args db "error, only one argument is needed. Syntax : ./main file", 0xA, 0
+	error_msg_file db "error, the file printed is a folder. Please enter an ELF file.", 0xA, 0
 	
 section .bss 
 	stat_buff resb 144 ;save space (144bytes) for the fstat command later
+	filename resb 256
 	
 section .text
     global _start
 
 _start:
-	mov rax, 2                  ; sys_open
-	mov rdi, filename      
-	mov rsi, 0                ; read-only
+	mov rdi, [rsp]          ; get argc
+	cmp rdi, 2              ; argc must be equal to 2 (name of the program + first argument) 
+	jne wrong_args 
+	mov rsi, [rsp +16]    ; getting the first argument 
+	mov rdi, filename    ; filename buffer in rdi 
+	call copy_string 
+	call main
+	call exit_program
+
+
+wrong_args:
+    mov rsi, error_msg_args  
+    mov rdx, 62
+    call print_message
+    mov rax, 60
+    mov rdi, 1
+    syscall
+
+
+main: 
+    call stat_file	
+    call check_if_regular_file
+    call check_elf
+    call close_file
+    ret
+
+
+
+stat_file:
+    mov rax, 4      ; sys_stat
+    mov rdi, filename
+    mov rsi, stat_buff
+    syscall
+    test rax, rax
+    js error
+    ret
+
+
+	
+check_if_regular_file:
+    mov rax, [stat_buff + 24]  ; load st_mode (16 bytes in the stat structure)
+    and rax, 0xF000            ; mask to isolate the mask type bites (the "file type" bytes are located in the first 4 bytes) 
+    cmp rax, 0x8000            ; 0x8000 = S_IFREG (regular file)
+    je is_regular_file
+    
+    mov rsi, error_msg_file
+    mov rdx, 63
+    call print_message
+    jmp exit_program
+
+is_regular_file:
+	ret
+
+
+check_elf:
+	; Open the file
+	mov rax, 2      ; sys_open
+	mov rdi, filename
+	mov rsi, 0      ; read-only
 	syscall
 	test rax, rax
-	js error                     ; Jump if SF=1 
-
-
-	mov rbx, rax                 ; File descriptor is contained in rax after the syscall, we save it in rbx
-	
-	mov rsi, stat_buff
-	mov rax, 0x5   ; sys_fstat
-	mov rdi, rbx
-	syscall        
-	test rax, rax
-	js error                     
-	
-	
-	mov rax, [stat_buff + 16]  ;load st_mode (16 bytes in the stat structure) 
-	and rax, 0xF000           ; mask to isolate the mask type bites (the "file type" bytes are located in the first 4 bytes) 
-	cmp rax, 0x4000           ; 0x4000 = S_IFDIR
-	je is_Directory
-	jmp not_Directory
-	
-	
-is_Directory:
-	;Print that the file is a Directory
-	mov rax, 0x1
-	mov rdi, 1
-	mov rsi, is_Directory_msg
-	mov rdx, 24
-	syscall
-	jmp _end
-	
-	
-	
-not_Directory:	
-
-	;Print that the file is not a Directory
-	mov rax, 0x1
-	mov rdi, 1
-	mov rsi, not_Directory_msg
-	mov rdx, 28
-	syscall
-
+	js error
+	mov rbx, rax    ; Save file descriptor
 
 
 	; read the first 4 bytes in the file 
@@ -72,44 +90,65 @@ not_Directory:
 	js error
 	
 	mov rcx, 4                   ; for the loop 
-	xor rdi, rdi        
+	xor rdi, rdi     
+	
+	call compare_loop
+	
+	mov rsi, is_ELF_msg
+	mov rdx, 25   
+	call print_message  
+	ret
 	
 
-	   
-print_loop: 
+compare_loop: 
 	;verify file magic bytes
 	mov al, [buf + rdi]
 	cmp al, [ELF_numbers + rdi]
 	jne not_ELF
 	inc rdi
-	loop print_loop
-		
-		
-	;Print that the file is an ELF
-	mov rax, 0x1
-	mov rdi, 1 ;file descriptor
-	mov rsi, is_ELF_msg
-	mov rdx, 25    ;msg length
-	syscall   
-	jmp _end
-
+	dec rcx
+    	jnz compare_loop
+	ret
 
 not_ELF:
 	;Print that the file is not an ELF
-	mov rax, 0x1
-	mov rdi, 1 ;file descriptor
 	mov rsi, not_ELF_msg
-	mov rdx, 29
-	syscall
+	mov rdx, 44
+	call print_message
+	jmp exit_program 
 
 
 
-_end:			
-	mov rdi, [stat_buff +0]     
+
+close_file:		
+	mov rdi, rbx    
 	mov rax, 3      ;sys_close 
 	syscall        
+	ret
 
 
+
+copy_string:
+    mov al, [rsi]       
+    mov [rdi], al      
+    test al, al        ; check if the caracter is null
+    je copy_done       
+    inc rsi             
+    inc rdi           
+    jmp copy_string     
+
+copy_done:
+    ret  
+    
+    
+    
+print_message:
+    mov rax, 0x1               
+    mov rdi, 1                 
+    syscall
+    ret
+
+exit_program:
 	mov rax, 60                  
 	xor rdi, rdi                
 	syscall
@@ -120,7 +159,7 @@ error:
 	mov rax, 0x1
 	mov rdi, 1 
 	mov rsi, error_msg
-	mov rdx, 30
+	mov rdx, 31
 	syscall
 	mov rax, 60                 
 	mov rdi, 1                    ; Exit code 1
